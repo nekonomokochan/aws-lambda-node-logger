143da5c60fdac7cad6febf03536052e107d2f05b
keitakn <keita.koga.work@gmail.com>
2019-05-31 19:19:17 +0900

    :recycle: #35 Adjusted because ESL Int's rules were strict

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,90 +1,30 @@
 module.exports = {
   env: {
-    browser: true,
-    es6: true,
     node: true,
-    'jest/globals': true
+    es6: true,
+    jest: true
   },
   extends: [
-    'airbnb',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:import/errors',
-    'plugin:import/warnings',
-    'plugin:prettier/recommended',
-    'prettier',
-    'prettier/@typescript-eslint'
+    "eslint:recommended",
+    "plugin:prettier/recommended"
   ],
-  globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
-    '__DEV__': true
-  },
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    project: './tsconfig.json',
-    sourceType: 'module',
-    ecmaVersion: 2019
-  },
   plugins: [
-    '@typescript-eslint',
-    'jest',
-    'prettier',
-    'prefer-arrow'
+    "@typescript-eslint"
   ],
-  root: true,
-  settings: {
-    'import/resolver': {
-      node: {
-        extensions: ['.js', 'jsx', '.ts', '.tsx']
-      }
-    }
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    "sourceType": "module",
+    "project": "./tsconfig.json",
+    "ecmaVersion": 2019
   },
   rules: {
-    // eslint official
-    'newline-before-return': 'error',
-    'no-console': 'warn',
-    'require-yield': 'error',
-
-    // @typescript-eslint
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/explicit-member-accessibility': 'off',
-    indent: 'off',
-    '@typescript-eslint/indent': 'off',
-    '@typescript-eslint/no-unnecessary-type-assertion': 'error',
-
-    // prefer-arrow
-    'prefer-arrow/prefer-arrow-functions': [
-      'error',
-      {
-        disallowPrototype: true,
-        singleReturnOnly: true,
-        classPropertiesAllowed: false
-      }
-    ],
-
-    // import
-    'import/extensions': [
-      'error',
-      'always',
-      {
-        js: 'never',
-        jsx: 'never',
-        ts: 'never',
-        tsx: 'never'
-      }
-    ],
-    'import/prefer-default-export': 'off',
-
-    // prettier
-    'prettier/prettier': [
-      'error', {
-        bracketSpacing: true,
-        printWidth: 80,
-        semi: true,
-        singleQuote: true,
-        trailingComma: 'all',
-        useTabs: false
-      }
-    ]
+    "no-console": 0,
+    "no-unused-vars": 0,
+    "no-undef": 0
+  },
+  settings: {
+    node: {
+      tryExtensions: [".ts", ".tsx", ".js", ".jsx", ".json", ".node"]
+    }
   }
 };
